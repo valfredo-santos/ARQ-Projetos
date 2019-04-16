7302f0c4a08394317224ae7ca597fc8954a021d3
Valfredo Santos Júnior <valfredojr3@gmail.com>
2019-04-16 18:22:38 -0300

    Programação Paralela

---
+++ ParallelMain.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <omp.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    //Leitura da imagem
+    Mat image = imread("test17.jpg", IMREAD_COLOR);
+
+    //Contagem de Tempo
+    clock_t tStart = clock();
+
+    //Aplicando o Filtro Negativo
+    #pragma omp parallel default(none) shared(image)
+	{
+        #pragma omp for
+        for (int y = 0; y < image.rows; y++)
+        {
+            for (int x = 0; x < image.cols; x++)
+            {
+                for (int channel = 0; channel < image.channels(); channel++)
+                {
+                    image.at<Vec3b>(y, x)[channel] = 255 - image.at<Vec3b>(y, x)[channel];
+                }
+            }
+        }
+	}
+
+    //Apresenta a Contagem de Tempo
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    //Apresenta a Imagem
+	namedWindow("Display window", WINDOW_NORMAL);
+    imshow("Display window", image);
+    waitKey();
+
+    return 0;
+}
